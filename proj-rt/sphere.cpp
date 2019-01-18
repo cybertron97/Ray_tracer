6647449ae6b120441532a28cfcd3065d0621eceb
Tanish Arora <taror002@bolt.cs.ucr.edu>
2019-01-17 17:53:26 -0800

    updassdgcdsggvx

--- proj-rt/sphere.cpp
+++ proj-rt/sphere.cpp
@@ -4,14 +4,32 @@
 // Determine if the ray intersects with the sphere
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
-    TODO;
-    return {0,0,0};
+    //TODO;
+    
+Hit hit; 
+vec3 u = ray.endpoint -center; 
+
+double g = pow(dot(u, ray.direction),2)-(u.magnitude()-pow(radius,2));  
+double t = dot(u, ray.direction)-sqrt(g); 
+if (g>=0) 
+{
+hit.dist=t;
+return hit; 
+
+
+}
+
+
+
+
+return {0,0,0};
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
 {
     vec3 normal;
-    TODO; // compute the normal direction
+    normal = point - center; 
+    //TODO; // compute the normal direction
     return normal;
 }
 
