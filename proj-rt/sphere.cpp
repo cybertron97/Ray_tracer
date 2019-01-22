7b491f35d4dd2f72f83b6f259a61558249c36e3a
Tanish Arora <taror002@bolt.cs.ucr.edu>
2019-01-21 18:34:21 -0800

    sphere

--- proj-rt/sphere.cpp
+++ proj-rt/sphere.cpp
@@ -5,30 +5,36 @@
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
     //TODO;
-    
-Hit hit; 
-vec3 u = ray.endpoint -center; 
-
-double g = pow(dot(u, ray.direction),2)-(u.magnitude()-pow(radius,2));  
-double t = dot(u, ray.direction)-sqrt(g); 
-if (g>=0) 
+    Hit res;
+    res.dist =0; 
+vec3 diff = ray.endpoint - center;
+double d = dot(ray.direction, diff);
+double a = dot (ray.direction, ray.direction);
+double square_root = sqrt(pow(d,2) -  (diff.magnitude_squared() - pow(radius,2))); //follow the formula
+double t_1= d + square_root;
+double t_2 = d - square_root;
+if (square_root < 0)
 {
-hit.dist=t;
-return hit; 
+  res.dist = -1;
 
+}
+else if (t_1 > 0.0001 || t_2 > 0.0001 )
+{
+  if (t_1>t_2)
+  {
+    res.dist = t_1;
+  }
+  else res.dist =t_2;
 
 }
 
-
-
-
-return {0,0,0};
+return res ;
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
 {
     vec3 normal;
-    normal = point - center; 
+    normal = point - center;
     //TODO; // compute the normal direction
     return normal;
 }
