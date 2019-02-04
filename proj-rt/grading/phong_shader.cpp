75706c28ddc73fcc0a1f5688f2c5d95c7759029e
Tanish Arora <taror002@bolt.cs.ucr.edu>
2019-02-03 18:08:04 -0800

    CP2

--- proj-rt/grading/phong_shader.cpp
+++ proj-rt/grading/phong_shader.cpp
@@ -9,19 +9,20 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color, colorA;
+    //ambient
     colorA += world.ambient_color * world.ambient_intensity * color_ambient;
     vec3 colorS;
     vec3 colorD;
 
-  for(int i = 0; i < world.lights.size(); i++)
+  for(unsigned int i = 0; i < world.lights.size(); i++)
       {
 
          vec3 light_intersect = (-intersection_point + (world.lights[i]->position));
         // std :: cout << "light_intersect " << l_decay <<std ::endl;
          vec3 L = world.lights[i]->Emitted_Light((light_intersect)); //L doesnt change for specular
-         vec3 n = normal.normalized();
+         vec3 n = normal.normalized(); //getting a seg fault
 		     vec3 l = light_intersect.normalized();
-        //calculating diffuse
+        //calc diffuse
         double ln;
     		if(dot(l,n) < 0)
         {
@@ -39,21 +40,25 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
         //   //std:: cout << "colord" << colorD << std ::endl;
         // }
         //color  = colorA + colorD;
+
        //specular
       vec3 R = (l - n * (2 * dot(l,n))).normalized();
       vec3 C = (intersection_point - ray.endpoint).normalized();
       double  RC;
 		if(dot(R,C) < 0)
     {
-			RC = 0;
+			RC = 0; //if less than the max then set to zero else make it equivalent to dot product
 		}
 		else
-    { RC = dot(R,C);
+    {
+      RC = dot(R,C);
      }
           double specular_intensity = pow(RC, specular_power);
           colorS += color_specular * L * specular_intensity;}
 //std:: cout << "colord" << colorD << std ::endl;
-color  = colorA + colorD + colorS;
+//std:: cout << "colors" << colorS << std ::endl;
+
+    color  = colorA + colorD + colorS;
     //TODO; //determine the color
 
     return color;
